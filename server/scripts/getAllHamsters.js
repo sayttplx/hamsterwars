3b02aa3cd2a108629cb13cab4ef99af17cdee17f
sayttplx <you@example.com>
2021-09-14 21:08:21 +0200

    firebase setup and scripts

---
+++ server/scripts/getAllHamsters.js
@@ -0,0 +1,31 @@
+const { db } = require('../firebase')
+const HAMSTERS = 'hamsters'
+
+getAllDocuments();
+
+async function getAllDocuments() {
+    console.log('Getting all hamsters...');
+
+    const hamstersRef = db.collection(HAMSTERS) 
+
+    const hamstersSnapshot = await hamstersRef.get()            
+
+    if (hamstersSnapshot.empty) {
+        console.log('No hamsters found.');
+        return
+    }
+    
+    const arr = []
+
+    await hamstersSnapshot.forEach(async hamstersRef => {
+
+        const hamster = await hamstersRef.data()
+        console.log('hamster', hamster)
+        hamster.id = hamstersRef.id
+        console.log('hamster id', hamster.id)
+        arr.push(hamster)
+    })
+
+    console.log('Hamsters from database:', arr);
+    return arr
+}
